cd356311b78cbb6119d1d6d0c8fcc0cecf795209
nagarajhegde <hegde4universe@gmail.com>
2022-08-13 17:14:00 +0530

    problem statement

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,6 +1,19 @@
 public class RansomNote {
 
+   /* Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
+    Each letter in magazine can only be used once in ransomNote.
+    Example1
+    Input: ransomNote = "aa", magazine = "ab"
+    Output: false
+
+    Example 2:
+
+    Input: ransomNote = "aa", magazine = "aab"
+    Output: true
+    */
+
     public static void main(String[] args) {
         System.out.println("Ransom Note");
+
     }
 }
