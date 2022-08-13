dd32e7f549cbe684433ef32c55a29dc12e868d47
nagarajhegde <hegde4universe@gmail.com>
2022-08-14 01:10:14 +0530

    solution

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class RansomNote {
 
    /* Given two strings ransomNote and magazine, return true if ransomNote can be constructed by using the letters from magazine and false otherwise.
@@ -8,12 +11,30 @@ public class RansomNote {
 
     Example 2:
 
-    Input: ransomNote = "aa", magazine = "aab"
-    Output: true
+    Input: ransomNote = "image", magazine = "magazine"   Output: true
+    Input: ransomNote = "imagine", magazine = "magazine"   Output: false (i cannot be used twice)
+
     */
 
     public static void main(String[] args) {
-        System.out.println("Ransom Note");
 
+        System.out.println(isConstructable("aa", "aab"));
+        System.out.println(isConstructable("aa", "ab"));
+        System.out.println(isConstructable("imagine", "magazine"));
+    }
+
+    private static boolean isConstructable(String ransomNote, String magazine) {
+        Map<Character, Integer> countMap = new HashMap<>();
+        for (char c : magazine.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            if (!countMap.containsKey(c) || countMap.get(c) <= 0) {
+                return false;
+            }
+            countMap.put(c, countMap.get(c) - 1);
+        }
+        return true;
     }
 }
